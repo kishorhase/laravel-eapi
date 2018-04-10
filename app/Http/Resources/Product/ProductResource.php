c7e37b74f94c68958ac6f3c9e4738775df8afe36
kishorhase <kishorhase@gmail.com>
2018-04-10 16:31:34 +0530

    Create API

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource {
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function toArray($request) {
+		return [
+			'name'        => $this->name,
+			'description' => $this->detail,
+			'price'       => $this->price,
+			'final_price' => round((1-$this->discount/100)*$this->price, 2),
+			'stock'       => $this->stock == 0?'Out of stock':$this->stock,
+			'discount'    => $this->discount,
+			'rating'      => $this->reviews->count() > 0?round($this->reviews->sum('star')/$this->reviews->count(), 2):'',
+			'href'        => [
+				'link'       => route('reviews.index', $this->id),
+			],
+		];
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,9 @@ Route::middleware('auth:api')->get('/user',
 function (Request $request) {
 		return $request->user();
 	});
+
 Route::apiResource('/products', 'ProductController');
+
 Route::group(['prefix' => 'products'], function () {
 		Route::apiResource('/{product}/reviews', 'ReviewController');
 	});
