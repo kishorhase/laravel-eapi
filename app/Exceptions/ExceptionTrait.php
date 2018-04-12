e00ce3d4cda5ee7c4e6521900f4dc2f4b237138f
kishorhase <kishorhase@gmail.com>
2018-04-12 15:46:56 +0530

    Exception handling added

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Exceptions;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+trait ExceptionTrait {
+
+	public
+
+function apiException($request, $e) {
+		if ($this->isModel($e)) {
+
+			return $this->modelResponse($e);
+		}
+
+		if ($this->isHttp($e)) {
+
+			return $this->httpResponse($e);
+		}
+
+		return parent::render($request, $exception);
+	}
+
+	protected function isModel($e) {
+
+		return $e instanceof ModelNotFoundException;
+
+	}
+
+	protected function isHttp($e) {
+		return $e instanceof NotFoundHttpException;
+
+	}
+	protected function modelResponse($e) {
+		return response()->json([
+				'errors' => 'Model Not Found',
+			], Response::HTTP_NOT_FOUND);
+
+	}
+
+	protected function httpResponse($e) {
+		return response()->json([
+				'errors' => 'Invalid route',
+			], Response::HTTP_NOT_FOUND);
+	}
+
+}
